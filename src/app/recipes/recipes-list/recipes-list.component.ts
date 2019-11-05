b0321ce5aa39dc36d3c2a3b113bfe0c0a57708f0
MacPRO <mac@Katerinas-MacBook-Pro.local>
2019-11-05 10:14:23 +0300

    Added basic project.

---
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipes-list',
+  templateUrl: './recipes-list.component.html',
+  styleUrls: ['./recipes-list.component.css']
+})
+export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [new Recipe
+    ('Berry', 'Berry', 'https://www.shutterstock.com/blog/wp-content/uploads/sites/5/2017/04/shutterstock_393753934.jpg?w=1024&h=682'),
+    new Recipe('Orange', 'Orange', 'https://cdn.fstoppers.com/styles/full/s3/media/2017/09/11/autumn_food_photography_0.jpg'),
+    new Recipe('Eggs', 'Eggs', 'https://i.pinimg.com/originals/b8/15/b0/b815b00b9a10bcc88a58d88ab49734a2.jpg')
+  ];
+  
+  constructor() { }
+
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredients.model';
+
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  ingredients = [
+    new Ingredient('Apples' , 5),
+    new Ingredient('Lemon' , 32),
+    new Ingredient('Cucumber' , 50)
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
